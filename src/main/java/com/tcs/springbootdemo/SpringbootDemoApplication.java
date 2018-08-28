76497cdb24d18c324ae512a84c3b8070a6b814d0
raulmq <raulmendozaq@gmail.com>
2018-08-27 23:00:27 -0500

    quinta version

--- src/main/java/com/tcs/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/tcs/springbootdemo/SpringbootDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
-	// file prueba 01
+	// file prueba 02
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
